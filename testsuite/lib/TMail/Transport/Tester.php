270823adaa3f2ac1115b1983f41fdcca8d0ab984
Bob Siefkes <bob@shopimport.nl>
2012-03-13 12:56:00 +0100

    Remove testsuite samples

--- testsuite/lib/TMail/Transport/Tester.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class TMail_Transport_Tester extends TMail_Transport {
-    function send($to,$from,$subject,$body,$headers){
-        $this->owner->owner->toutput=$headers.$body;
-    }
-}
--- testsuite/lib/helloworld/Core.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace helloworld;
-class Core extends \AbstractController {
-}
