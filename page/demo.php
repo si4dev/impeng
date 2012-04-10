a8b6716a03bf84f4c2962cf2d876622f495a8f03
Bob Siefkes <bob@shopimport.nl>
2012-04-10 16:27:16 +0200

    added demo for debugging something

--- page/demo.php
+++ page/demo.php
@@ -1,9 +1,26 @@
 <?php
+class Model_Demo extends Model_Table {
+  public $table='demo';
+  function init() {
+    parent::init();
+    $this->addField('sometext');
+    $this->addField('age');
+    //$this->addField('title');
+  }
+}
 
 class page_demo extends Page {
   function init() {
     parent::init();
 
+
+$user=$this->add('Model_Demo')->debug();
+$user->addCondition('age','>','20');
+$user->set('age',11);
+$user->saveAndUnload();           // exception!
+echo '++'.$user->id;
+
+/*
     $c=$this->add('CRUD');
     $c->setModel('Model_Shop');
     if ($c->grid){ 
@@ -11,6 +28,8 @@ class page_demo extends Page {
         $c->grid->addQuickSearch(array('name'));
         $c->grid->addPaginator(10);
     }
+*/
+  
   }
     
 }
