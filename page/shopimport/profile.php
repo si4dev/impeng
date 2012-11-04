cbf13fe51aa14b8e041a61c3a1511d591d3635fb
herizo <herizo.ludovic@gmail.com>
2012-11-04 12:49:03 +0100

    add success message to profile form

--- page/shopimport/profile.php
+++ page/shopimport/profile.php
@@ -5,16 +5,23 @@ Class Page_shopimport_profile extends Page {
 	  parent::init();
 		$si=$this->add('Controller_Shopimport');
 		$shop=$si->shop;
+		
 		$f=$this->add('Form');
-		$d=$f->addField('line', 'domain')->set($shop->shopconfig('domain'));
+		$d=$f->addField('line', 'domain')->setCaption('Domain :');
+		$d->set($shop->shopconfig('domain'));		
 		$d->addClass('span3');
-		$e=$f->addField('line', 'email')->set($shop->shopconfig('email'));
+		
+		$e=$f->addField('line', 'email')->setCaption('Email :');
+		$e->set($shop->shopconfig('email'));
 		$e->addClass('span3');
+		
 		$f->addSubmit();
+		
 		if($f->isSubmitted()){
 			$shop->shopconfig('domain', $f->get('domain'));
 			$shop->shopconfig('email', $f->get('email'));
 			$shop->save();
+			$f->js()->univ()->successMessage('Changes Saved')->execute();
 		}
 	}
 }
