6758681874e640383c915e30a6ef865473f00391
Bob Siefkes <bob@shopimport.nl>
2012-11-17 16:05:59 +0100

    Supplier file upload

--- lib/Model/uploadPricelist.php
+++ lib/Model/uploadPricelist.php
@@ -1,10 +1,19 @@
 <?php 
 
 Class Model_UploadPricelist extends filestore\Model_file {
-	
+
+  function init() {
+    parent::init();
+    
+    $this->addHook('beforeGenerateFilename',$this);
+  }
+  function beforeGenerateFilename($o) {
+    $o->set('filename',$this->supplierpath);
+  }
+  	
 	function getPath(){
 		return $this->supplierpath;		
 	}
-	function generateFilename(){
-	}
+    
+  
 }
