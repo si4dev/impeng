458aeebfa89b3bc1a37fc7d326458da558655b52
Bob Siefkes <bob@shopimport.nl>
2012-11-28 13:30:17 +0100

    corrections on dbug

--- lib/Dbug.php
+++ lib/Dbug.php
@@ -1,61 +1,9 @@
 <?php
-// -------------------------------------------------------------------------------------------------
-// error handling, generate events on all except notice.
-// the default handler will be redirected also to capture errors to mysql
-// when dbug switch is enabled then display_errors will be true later on
-
-//error_reporting(E_ALL ^ E_NOTICE);
-//ini_set("display_errors", 0);
-
-// set the error handler to catch all the php errors
-//libxml_use_internal_errors(true);
-//register_shutdown_function('shutdown');
-
-// -------------------------------------------------------------------------------------------------
-// rethrow php errors 
-
-
-if(!function_exists('exceptions_error_handler')){
-function exceptions_error_handler($errno, $errstr, $errfile, $errline) {
-  $errorType = Array (
-                E_ERROR               => "Error",
-                E_WARNING             => "Warning", // 8
-                E_PARSE               => "Parsing Error",
-                E_NOTICE              => "Notice",
-                E_CORE_ERROR          => "Core Error",
-                E_CORE_WARNING        => "Core Warning",
-                E_COMPILE_ERROR       => "Compile Error",
-                E_COMPILE_WARNING     => "Compile Warning",
-                E_USER_ERROR          => "User Error",
-                E_USER_WARNING        => "User Warning",
-                E_USER_NOTICE         => "User Notice",
-                4096                  => "Runtime Notice"
-                );
-  echo "[[$errno:$errstr]]";
-  switch ($errno) {
-    case 2:
-      if(strpos($errstr,'mysql_connect')!==false)break;
-    case 8:
-      if(strpos($errstr,'Undefined offset') !==false)break;
-      if(strpos($errstr,'Undefined index') !==false)break;
-      if(strpos($errstr,'Undefined property') !==false)break;
-      if(strpos($errstr,'in_array() expects parameter 2 to be array, null given') !==false)break;
-    case 2048:
-      if(strpos($errstr,'var: Deprecated') !==false)break;
-      if(strpos($errstr,'Declaration of ') !==false)break;
-      if(strpos($errstr,'Non-static method') !==false)break;
-    case 8192:
-      if(strpos($errstr,'is deprecated') !==false)break;
-    default:
-      throw new ErrorException($errorType[$errno].': ['.$errfile.':'.$errline.']'. $errstr, 0, $errno, $errfile, $errline);
-      break;
-  }
-}
-}    
-//set_error_handler('exceptions_error_handler');
-
-//throw new ErrorException('hello', 0, 12,'file',123);
 	
+// [1] run $this->add('Dbug') in Frontend BEFORE parent::init() to get correct sequence of caughtException hook
+//     as Logger.php will exit and then Dbug hook would not be processed
+// [2] ensure to have a database connection before calling this class as it uses the Log model 
+
 class Dbug extends AbstractController {
 
   public $logmsg = '';
@@ -63,10 +11,8 @@ class Dbug extends AbstractController {
   function init() {
     parent::init();
 
-    // ensure to have database
-    $this->setModel('Log');
-    $this->model->start();
-    //$this->model->end();
+    // set and start model
+    $this->setModel('Log')->start();
 
     //remove old hooks before add the new hooks
     $this->api->removeHook('caught-exception');
@@ -79,11 +25,16 @@ class Dbug extends AbstractController {
   function caughtException($caller,$e){
      $error = $e->getMessage();
      $error .= $this->backtrace($e->shift, $e->getTrace());
-     $this->model->start();
-     $this->model->end($error);     
+     $this->model->set('error',$error);     
         
   }  
 
+  // -----------------------------------------------------------------------------------------------
+  // on descrturct log the duratoin and (max) memory used
+  function __destruct(){
+    $this->model->end();
+    parent::__destruct();
+  }
 
 	function set($msg){        
     $this->logmsg .= $msg;    
--- lib/Frontend.php
+++ lib/Frontend.php
@@ -5,7 +5,12 @@ class Frontend extends ApiFrontend {
   protected $shop;
   function md5($value) {return md5($value);}
 	function init(){
+    
+    $this->dbConnect();
+    $this->add('Dbug');
+     
 		parent::init();
+	
 		$this->addLocation('atk4-addons',array(
 					'php'=>array(
                         'mvc',
@@ -19,13 +24,6 @@ class Frontend extends ApiFrontend {
 			->_load('atk4_univ')
 			// ->_load('ui.atk4_expander')
 			;
-
-    $this->dbConnect();
-   
-   
-   $this->add('Logger');
-	
-	
 
     $this->add('Auth')->setModel('User'); // email and password are default to login
     //$this->auth->usePasswordEncryption('md5')->check();
@@ -50,7 +48,8 @@ class Frontend extends ApiFrontend {
 			throw new exception("Attempt to hack");
 		}
 	 }
-	
+   
+  
   				 					
    //debug mode for logMsg
     $url = $this->api->getDestinationURL($this->api->url('/'));
