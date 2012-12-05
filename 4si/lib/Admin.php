3afb631e723c4fd6b314b1dfe48de699501b4724
Bob Siefkes <bob@shopimport.nl>
2012-12-05 13:51:38 +0100

    reverse of deletion of admin file

---
+++ 4si/lib/Admin.php
@@ -0,0 +1,44 @@
+<?php
+class Admin extends ApiFrontend {
+  public $is_admin=true;
+  function init() {
+    ini_set('display_errors',1);
+    
+    $this->addLocation('..',array(
+                    'php'=>array(
+                        'lib',
+                        'atk4-addons/mvc',
+                        'atk4-addons/billing/lib',
+                        'atk4-addons/misc/lib',	
+                        )
+						,
+					'addons' => 'atk4-addons'
+                    ))
+            ->setParent($this->pathfinder->base_location);
+
+    $this->dbConnect();
+    $this->add('Dbug');
+    
+    parent::init();
+	
+    $this->add('jUI');
+
+    $this->js()
+            ->_load('atk4_univ')
+            ->_load('ui.atk4_notify');
+
+    $this->add('Auth')->setModel('Admin'); // email and password are default to login
+    $this->auth->usePasswordEncryption(function($v) { return md5($v); } );
+    $this->auth->check();
+
+    
+    $m = $this->add('Menu', null, 'Menu');
+    $m->addMenuItem('user','Users');      
+    $m->addMenuItem('shop','Shops');      
+    $m->addMenuItem('supplier','Suppliers');
+  	$m->addMenuItem('fileadmin', 'Manage Uploads');
+    $m->addMenuItem('errors', 'Error logs');
+    $m->addMenuItem('logout','Log out'); 
+
+  }
+}
